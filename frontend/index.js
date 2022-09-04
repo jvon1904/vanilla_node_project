03ea63d526d2417dfd07ad15a73c1e0c5189caa0
Jesse vonBergen <jvon1904@gmail.com>
2022-09-04 19:05:24 -0400

    complete PUT functionality

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,8 @@
   const productForm = document.getElementById("product-form");
   const productSubmit = document.getElementById("product-submit");
 
+  document.addEventListener("DOMContentLoaded", () => fetchProducts());
+
   class Product {
     constructor({ id, name, description, price }) {
       this.id = id;
@@ -24,6 +26,12 @@
       );
       removeButton.onclick = removeProduct;
 
+      const editButton = document.createElement("div");
+      editButton.classList.add("btn-edit", "hidden");
+      editButton.append(document.createElement("span"));
+      editButton.childNodes[0].innerText = "Edit";
+      editButton.onclick = editProduct;
+
       const productAttributes = document.createElement("div");
       productAttributes.classList.add("product-attributes");
 
@@ -41,38 +49,100 @@
 
       productAttributes.append(name, description, price);
 
-      productContainer.append(removeButton, productAttributes);
-      productsContainer.appendChild(productContainer);
+      productContainer.append(removeButton, editButton, productAttributes);
+      productContainer.onmouseover = (e) => {
+        if (e.target.classList.contains("product-container")) {
+          const editButton = e.target.childNodes[1];
+          editButton.classList.remove("hidden");
+        }
+      };
+      productContainer.onmouseleave = (e) => {
+        if (e.target.classList.contains("product-container")) {
+          const editButton = e.target.childNodes[1];
+          editButton.classList.add("hidden");
+        }
+      };
+      productsContainer.prepend(productContainer);
     }
   }
 
   productSubmit.addEventListener("click", async () => {
     const productData = new FormData(productForm);
+    clearForm(productForm);
     const productObject = {};
     productObject["name"] = productData.get("name");
     productObject["description"] = productData.get("description");
     productObject["price"] = productData.get("price");
+    createProduct(productObject);
+  });
+
+  function fetchProducts() {
+    fetch("http://127.0.0.1:8080/api/products")
+      .then((resp) => resp.json())
+      .then((json) => displayProducts(json.data));
+  }
+
+  function displayProducts(products) {
+    const productsContainer = document.getElementById("products-container");
+    for (d of products) {
+      const product = new Product(d);
+      product.append();
+    }
+  }
+
+  async function createProduct(data) {
     const result = await fetch("http://127.0.0.1:8080/api/products", {
       method: "POST",
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(productObject),
+      body: JSON.stringify(data),
     })
       .then((resp) => resp.json())
       .then((json) => displayProducts(json.data));
-  });
+  }
 
-  fetch("http://127.0.0.1:8080/api/products")
-    .then((resp) => resp.json())
-    .then((json) => displayProducts(json.data));
-
-  function displayProducts(products) {
-    for (d of products) {
-      const product = new Product(d);
-      product.append();
+  function editProduct(e) {
+    const target = e.target.classList.contains("btn-edit")
+      ? e.target
+      : e.target.parentElement;
+    const container = target.parentElement;
+    if (target.childNodes[0].innerText === "Submit") {
+      return updateProduct(container);
     }
+    target.childNodes[0].innerText = "Submit";
+    for (p of container.childNodes[2].childNodes) {
+      const attribute = p.classList[0].split("-")[1];
+      const value = p.innerText.split(": ")[1];
+      const label = document.createElement("label");
+      label.innerText = attribute;
+      const input = document.createElement("input");
+      input.value = value;
+      p.innerText = "";
+      p.append(label, input);
+    }
+  }
+
+  async function updateProduct(container) {
+    const id = container.getAttribute("data-id");
+    const productObject = {};
+    for (p of container.childNodes[2].childNodes) {
+      const attribute = p.childNodes[0].innerText;
+      const value = p.childNodes[1].value;
+      productObject[attribute] = value;
+    }
+    const result = await fetch(`http://127.0.0.1:8080/api/products/${id}`, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productObject),
+    }).then((resp) => {
+      productsContainer.innerHTML = "";
+      fetchProducts();
+    });
   }
 
   function removeProduct(e) {
@@ -90,5 +160,15 @@
         container.remove();
       }, 490);
     });
+  }
+
+  function clearForm(form) {
+    for (div of form.children[0].children) {
+      for (element of div.children) {
+        if (element.tagName === "INPUT") {
+          element.value = "";
+        }
+      }
+    }
   }
 })();
